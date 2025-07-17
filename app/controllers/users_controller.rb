24af8308ff293385fa66c7953cb8a06ca6d3ed04
Tran Ngoc Hung <hungngtran04@gmail.com>
2025-07-17 17:47:52 +0700

    Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,25 @@ class UsersController < ApplicationController
 
   def show; end
 
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t(".success")
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def new
     @user = User.new
   end
 
   private
+
+  def user_params
+    params.require(:user).permit User::USER_PERMIT
+  end
 
   def load_user
     @user = User.find_by id: params[:id]
